24bbc392478256a55a36829e9057d0294f094675
Sebastian <sebastian@Sebastians-MacBook-Pro.local>
2019-11-04 12:09:21 -0500

    make it better

--- client/src/App/Espacios/Espacios.js
+++ client/src/App/Espacios/Espacios.js
@@ -1,26 +1,66 @@
-import React from 'react'
+import React from "react";
+
+import Container from "react-bootstrap/Container";
 
 export default class Espacios extends React.Component {
-    
-    constructor(props) {
-        super(props);
-    
-        this.state = {  }
-    }
+  constructor(props) {
+    super(props);
 
-    componentDidMount() {
-        
-    }
+    this.state = {
+      espacios: []
+    };
 
+    this.get_espacios = this.get_espacios.bind(this);
+  }
 
-    render() { 
-        return (
-            <div>
-                <h1>
-                    Espacios funciona
-                </h1>
-            </div>
-        );
-    }
+  componentDidMount() {
+    this.get_espacios();
+  }
+
+  get_espacios() {
+    fetch("http://localhost:5000/api/espacios", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        }
+        console.log(response.status, "The response was not OK");
+      })
+      .then(data => {
+        if (data) {
+          this.setState({ espacios: data });
+        }
+      })
+      .catch(err => {
+        console.log("Get Espacios Error ::", err.message);
+      });
+  }
+
+  render() {
+    return (
+      <div>
+        <div className="row">
+          {this.state.espacios.map((x, i) => {
+            return (
+              <div key={i} className="col-md-4" style={{ marginTop: "2em" }}>
+                <div className="card">
+                  <div className="card-body">
+                    <h5 className="card-title">{x.parqueadero}</h5>
+                    <p className="card-text">{x.descripcion}</p>
+                    <a href="#" className="btn btn-primary">
+                      Reservar
+                    </a>
+                  </div>
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
 }
- 
