a577f3115e5626fd7e314eb7d87d9aed555b94de
Juan Camilo Pulido <jc.puido@uniandes.edu.co>
2019-11-04 18:13:19 -0500

    mas arreglos

--- client/src/App/Home/Home.js
+++ client/src/App/Home/Home.js
@@ -1,7 +1,6 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import Cookies from 'universal-cookie';
-import { Link } from "react-router-dom";
 
 
 import "./Home.css";
