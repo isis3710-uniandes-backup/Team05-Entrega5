972edadefc40e895dfce4ef3b23ac4292c357fa4
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-11-04 17:57:01 -0500

    Cambios correcciones generales.

--- client/src/App/Registrar/Registrar.js
+++ client/src/App/Registrar/Registrar.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import NavBar from '../Navbar/Navbar';
 import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
