84619c202527fe915c4345603344696bbf2e5759
vianaernesto <le.viana@uniandes.edu.co>
2019-11-05 05:33:11 -0500

    Pagar, falta que haga post y mejorar como se ve

--- client/src/App/Pagos/Pagar.js
+++ client/src/App/Pagos/Pagar.js
@@ -3,9 +3,13 @@ import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import ButtonToolbar from 'react-bootstrap/ButtonToolbar';
+import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import { toast } from 'react-toastify';
 import Cookies from 'universal-cookie';
 import "./Pagar.css";
+import axios from 'axios';
 
 const cookies = new Cookies();
 
@@ -16,7 +20,18 @@ export default class Pagar extends Component {
         this.state = {
             idReserva :cookies.get('_idReserva'),
             idEspacio : cookies.get('_idEspacio'),
+            costo : 0,
+            metodo: "Tarjeta de Credito",
         };
+
+    }
+
+    componentDidMount(){
+        axios.get(`http://localhost:5000/api/espacios/${this.state.idEspacio}`)
+        .then(x =>{
+            this.setState({costo : x.data[0]})
+        })
+        .catch(err => toast.error(`Hubo un error obteniendo el costo :( -> ${err}`));
     }
 
     render(){
@@ -38,11 +53,20 @@ export default class Pagar extends Component {
                             <div className="pago-container">
                                 <Form className="text-left">
                                     <Form.Group>
-                                        <Form.Label>x</Form.Label>
+                                        <Form.Label>Escoje el metodo de pago:</Form.Label>
+                                        <ButtonToolbar aria-label="Opciones de Metodo de Pago">
+                                            <ButtonGroup vertical  size="lg">
+                                                <Button>Tarjeta de Crédito</Button>
+                                                <Button>Efectivo</Button>
+                                                <Button>Cuenta</Button>
+                                            </ButtonGroup>
+                                        </ButtonToolbar>
                                     </Form.Group>
+                                    <Button variant="success" size="md" block onClick={this.handleSubmit}>Pagar</Button>
                                 </Form>
                             </div>
                         </Col>
+                        <Col xs="0" sm="1" md="3" large="4" xl="4"></Col>
                     </Row>
                 </Container>
             </div>
