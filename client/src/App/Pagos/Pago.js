6d36dc150983786619e186eced50ee05842cb0fa
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-11-08 00:45:52 -0500

    Cambios en el pago que no quiere servir la fecha !!!

--- client/src/App/Pagos/Pago.js
+++ client/src/App/Pagos/Pago.js
@@ -6,11 +6,9 @@ export default class Pago extends Component {
     render() {
         return (
             <ul className="list-group list-group-flush mb-5">
-                 {
-                        (this.props.pagos.length === 0) ?
-                        <li>Parece que aún no has realizado pagos.</li> :
-                        this.props.pagos.map((e, i) => <PagoDetail i={i} pago={e} />)
-                    }
+                {
+                    this.props.pagos.map((e, i) => <PagoDetail i={i} pago={e} />)
+                }
             </ul>
         );
     }
--- client/src/App/Pagos/PagoDetail.js
+++ client/src/App/Pagos/PagoDetail.js
@@ -2,26 +2,19 @@ import React, { Component } from 'react';
 
 export default class PagoDetail extends Component {
 
-    constructor(props){
-        super(props);
-        this.state = {
-            fecha : (new Date(this.props.pago.fecha)).toString(),
-        };
-
-    }
     render() {
-        const fecha = new Date(this.props.pago.fecha);
+        const fecha = this.props.pago.fecha;
         let dia = fecha.getDay();
         let mes = fecha.getMonth();
         let anio = fecha.getFullYear();
 
         let horas = fecha.getHours();
         let minutos = fecha.getMinutes();
-        
+
         return (
             <li key={this.props.i} className="list-group-item d-flex flex-md-row flex-column justify-content-between align-items-center">
-                <img className="float-left" src={this.props.pago.imagenMetodoPago} alt="Icono método de pago" width="55" height="55"/>
-                <strong className="mt-3">Valor: {this.props.pago.cantidad}</strong>
+                <img src={this.props.pago.imagenMetodoPago} alt="Icono método de pago" width="55" height="55"/>
+                <strong>Valor: {this.props.pago.cantidad}</strong>
                 <strong>Fecha: {dia}/{mes}/{anio}</strong>
                 <strong>Hora: {horas}:{minutos}</strong>
             </li>
