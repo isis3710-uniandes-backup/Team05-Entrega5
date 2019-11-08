c8614034f382141a9654d7c1c91471b715d7bde9
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-11-08 00:48:23 -0500

    Cambios de nuevo para ver si así funciona.

--- client/src/App/Pagos/Pago.js
+++ client/src/App/Pagos/Pago.js
@@ -3,6 +3,10 @@ import PagoDetail from "./PagoDetail.js";
 
 export default class Pago extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         return (
             <ul className="list-group list-group-flush mb-5">
--- client/src/App/Pagos/PagoDetail.js
+++ client/src/App/Pagos/PagoDetail.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 export default class PagoDetail extends Component {
 
+    constructor(props) {
+        super(props);
+    }
+
     render() {
         const fecha = this.props.pago.fecha;
-        let dia = fecha.getDay();
+        let dia = fecha.getDate();
         let mes = fecha.getMonth();
         let anio = fecha.getFullYear();
 
