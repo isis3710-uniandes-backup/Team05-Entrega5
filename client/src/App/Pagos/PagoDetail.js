51c1f8f935d8444318b6662a829f1099b92127d9
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-11-08 00:32:42 -0500

    Cambios en el detalle de pagos porque con eso no sirve.

--- client/src/App/Pagos/PagoDetail.js
+++ client/src/App/Pagos/PagoDetail.js
@@ -10,12 +10,20 @@ export default class PagoDetail extends Component {
 
     }
     render() {
+        const fecha = new Date(this.props.pago.fecha);
+        let dia = fecha.getDay();
+        let mes = fecha.getMonth();
+        let anio = fecha.getFullYear();
+
+        let horas = fecha.getHours();
+        let minutos = fecha.getMinutes();
+        
         return (
             <li key={this.props.i} className="list-group-item d-flex flex-md-row flex-column justify-content-between align-items-center">
                 <img className="float-left" src={this.props.pago.imagenMetodoPago} alt="Icono método de pago" width="55" height="55"/>
                 <strong className="mt-3">Valor: {this.props.pago.cantidad}</strong>
-                <strong>Fecha: {this.state.fecha.split(" ")[2]}/{this.state.fecha.split(" ")[1]}/{this.state.fecha.split(" ")[3]}</strong>
-                <strong>Hora: {this.state.fecha.split(" ")[4].split(":")[0]}:{this.state.fecha.split(" ")[4].split(":")[1]}</strong>
+                <strong>Fecha: {dia}/{mes}/{anio}</strong>
+                <strong>Hora: {horas}:{minutos}</strong>
             </li>
         );
     }
