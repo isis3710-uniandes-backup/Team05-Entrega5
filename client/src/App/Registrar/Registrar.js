dcf7869ea054d31abe4fa71baf688417583db93a
Santiago Salazar <s.salazara@uniandes.edu.co>
2019-12-05 10:39:16 -0500

    Finalizada la internacionalización del registro!

--- client/src/App/Registrar/Registrar.js
+++ client/src/App/Registrar/Registrar.js
@@ -9,6 +9,7 @@ import Container from 'react-bootstrap/Container';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import { FormattedMessage } from 'react-intl';
 
 const validEmailRegex = RegExp(/^(([^<>()\]\\.,;:\s@"]+(\.[^<>()\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
 
@@ -59,25 +60,25 @@ export default class Registrar extends Component {
             case 'username':
                 errors.username =
                     value.length < 5
-                        ? 'El nombre de usuario debe tener por lo menos 5 caracteres'
+                        ? <FormattedMessage id="login.errorNombreUsuario" />
                         : '';
                 break;
             case 'email':
                 errors.email =
                     validEmailRegex.test(value)
                         ? ''
-                        : 'El correo ingresado no es válido';
+                        : <FormattedMessage id="registrarse.errorCorreo"/>;
                 break;
             case 'password':
                 errors.contrasenha =
                     value.length < 8
-                        ? 'La contraseña debe tener por lo menos 8 caracteres'
+                        ? <FormattedMessage id="registrarse.errorContrasenia" />
                         : '';
                 break;
             case 'nombre':
                 errors.nombre =
                     value.length < 5
-                        ? 'El nombre debe tener más de 5 caracteres'
+                        ? <FormattedMessage id="registrarse.errorNombre" />
                         : '';
                 break;
             default:
@@ -144,7 +145,7 @@ export default class Registrar extends Component {
         ).then(response => {
             if(response.data.success){
                 this.props.history.push('/login');
-                toast.success("¡Registro exitoso! Ahora inicia sesión");
+                toast.success(<FormattedMessage id="toast.exitoRegistro"/>);
             }
             else {
                 this.setState({
@@ -174,7 +175,7 @@ export default class Registrar extends Component {
                 contrasenha: this.state.contrasenha,
                 error: true
             });
-            toast.error("¡Hubo un error en el registro!");
+            toast.error(<FormattedMessage id="toast.errorRegistro"/>);
         }
     }
 
@@ -192,7 +193,7 @@ export default class Registrar extends Component {
         if (this.state.error) {
             incorrectMessage =
                 <Container className="error">
-                    Hay campos vacíos o algún campo tiene errores
+                    <FormattedMessage id="registrarse.errorCamposVacios" />
                 </Container>
         }
 
@@ -204,7 +205,7 @@ export default class Registrar extends Component {
                         <Col xs="0" sm="0" md="3" large="3" xl="3"></Col>
                         <Col xs="12" sm="12" md="6" large="6" xl="6">
                             <h1 className="title font-weight-bold med">
-                                Registrarse
+                                <FormattedMessage id="registrarse.tituloRegistrarse" />
                             </h1>
                         </Col>
                         <Col xs="0" sm="0" md="3" large="3" xl="3"></Col>
@@ -216,33 +217,33 @@ export default class Registrar extends Component {
                             <div className="border-container">
                                 <Container className="registrar-container">
                                     <Form className="text-left">
-                                        <small className="text-muted">Todos los campos con (*) son obligatorios</small>
+                                        <small className="text-muted"><FormattedMessage id="registrarse.info"/></small>
                                         <Form.Group>
-                                            <Form.Label htmlFor="username">Nombre de usuario *</Form.Label>
-                                            <Form.Control type="text" id="username" autoComplete="new-password" name="username" placeholder="(Ejemplo: userio)" title="Nombre usuario" onChange={this.handleUsername}></Form.Control>
+                                            <Form.Label htmlFor="username"><FormattedMessage id="registrarse.campoNombreUsuario"/> *</Form.Label>
+                                            <Form.Control type="text" id="username" autoComplete="new-password" name="username" placeholder="(e.g: userio)" onChange={this.handleUsername}></Form.Control>
                                             {errors.username.length > 0 &&
                                                 <span className='error'>{errors.username}</span>}
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label htmlFor="nombre">Nombre completo *</Form.Label>
-                                            <Form.Control type="name" id="nombre" name="nombre" placeholder="(Ejemplo: Pepito Pérez)" title="Nombre" onChange={this.handleNombre}></Form.Control>
+                                            <Form.Label htmlFor="nombre"><FormattedMessage id="registrarse.campoNombre"/> *</Form.Label>
+                                            <Form.Control type="name" id="nombre" name="nombre" placeholder="(e.g: Pepito Pérez)" onChange={this.handleNombre}></Form.Control>
                                             {errors.nombre.length > 0 &&
                                                 <span className='error'>{errors.nombre}</span>}
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label htmlFor="email">Correo *</Form.Label>
-                                            <Form.Control type="email" id="email" name="email" placeholder="(Ejemplo: correo@gmail.com)" title="Correo" onChange={this.handleCorreo}></Form.Control>
+                                            <Form.Label htmlFor="email"><FormattedMessage id="registrarse.campoCorreo"/> *</Form.Label>
+                                            <Form.Control type="email" id="email" name="email" placeholder="(e.g: correo@gmail.com)" onChange={this.handleCorreo}></Form.Control>
                                             {errors.email.length > 0 &&
                                                 <span className='error'>{errors.email}</span>}
                                         </Form.Group>
                                         <Form.Group>
-                                            <Form.Label htmlFor="password">Contraseña *</Form.Label>
-                                            <Form.Control type="password" id="password" autoComplete="new-password" name="password" placeholder="(Ejemplo: contra$enia)" title="Contraseña" onChange={this.handleContrasenha}></Form.Control>
+                                            <Form.Label htmlFor="password"><FormattedMessage id="registrarse.campoContrasenia"/> *</Form.Label>
+                                            <Form.Control type="password" id="password" autoComplete="new-password" name="password" placeholder="(e.g: contra$enia)" onChange={this.handleContrasenha}></Form.Control>
                                             {errors.contrasenha.length > 0 &&
                                                 <span className='error'>{errors.contrasenha}</span>}
                                         </Form.Group>
                                         <div className="d-flex justify-content-center pt-3">
-                                            <button type="submit" className="but-solid" onClick={this.handleSubmit}>Registrarse</button>
+                                            <button type="submit" className="but-solid" onClick={this.handleSubmit}><FormattedMessage id="registrarse.botonRegistrarse"/></button>
                                         </div>
                                     </Form>
                                 </Container>
@@ -253,7 +254,7 @@ export default class Registrar extends Component {
                     <Container className="cuenta-existente pb-4">
                         <Row>
                             <Col>
-                                <Link to="/login">¿Ya tienes una cuenta? Inicia sesión</Link>
+                                <Link to="/login"><FormattedMessage id="registrarse.mensajeIngresar"/></Link>
                             </Col>
                         </Row>
                     </Container>
